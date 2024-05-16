a87de3650bb1ace4eafe5c138dd65ca209196bcf
Hubert <hubert.maka02@gmail.com>
2024-05-16 16:23:19 +0200

    Update audio process class

--- src/preprocess/process_audio.py
+++ src/preprocess/process_audio.py
@@ -1,48 +1,67 @@
 import tensorflow as tf
-from src.preprocess.preprocess import Preprocess
+import random
+import librosa
 
 
 class ProcessAudio:
-    def __init__(self, wav_tensor: tf.Tensor, sample_rate: int):
-        self._wav_tensor = wav_tensor
+    def __init__(self, audio_tensor: tf.Tensor, sample_rate: int):
+        self._audio_tensor = audio_tensor
         self._sample_rate = sample_rate
-        self._time_probes = self._wav_tensor.shape[0]
+        self._time_probes = self._audio_tensor.shape[0]
+        self._audio_np = self._audio_tensor.numpy()
 
-    def _cut_audio(self, min_clip_duration_ms) -> tf.Tensor:
-        overlap = int((self._time_probes - (min_clip_duration_ms / 1000) * self._sample_rate) / 2)
-        return self._wav_tensor[overlap:(self._time_probes - overlap)]
+    def change_amplitude_rand(self, min_increase: float = 0.5, max_increase: float = 2.0):
+        increased_amp = self._audio_tensor * random.uniform(min_increase, max_increase)
+        return tf.convert_to_tensor(increased_amp, dtype=tf.float32)
 
-    def _add_zeros(self, min_clip_duration_ms) -> tf.Tensor:
-        missing_probes_side = int((min_clip_duration_ms / 1000 * self._sample_rate - self._time_probes) // 2)
-        return tf.pad(self._wav_tensor.numpy(), [[missing_probes_side, missing_probes_side]])
+    # TODO: do zmiany
+    def normalize_audio(self) -> tf.Tensor:
+        max_amplitude = tf.reduce_max(tf.abs(self._audio_tensor))
+        normalized_wav = self._audio_tensor / max_amplitude  # Normalizacja do zakresu [-1, 1]
+        return normalized_wav
 
-    def align_probes(self, min_clip_duration_ms) -> tf.Tensor:
-        expected_probes = (min_clip_duration_ms / 1000) * self._sample_rate
-        current_probes = self._time_probes
-        if expected_probes > current_probes:
-            return self._add_zeros(min_clip_duration_ms)
-        if expected_probes < current_probes:
-            return self._cut_audio(min_clip_duration_ms)
-        if expected_probes == current_probes:
-            return self._wav_tensor
+    def add_noise_rand(self, min_level: float = 0.1, max_level: float = 1.5) -> tf.Tensor:
+        noise_level = random.uniform(min_level, max_level)
+        return self._audio_tensor + tf.random.normal(tf.shape(self._audio_tensor), mean=0.0, stddev=noise_level)
 
-    def load_and_align_probes(self, file_path, min_clip_duration_ms):
-        wav = Preprocess.load_audio(file_path)
-        expected_probes = int((min_clip_duration_ms / 1000) * self._sample_rate)
-        print(expected_probes)
-        current_probes = wav.shape[0]
-        print(current_probes)
-        if expected_probes > current_probes:
-            print("Add zeros")
-            return self._add_zeros(min_clip_duration_ms)
-        elif expected_probes < current_probes:
-            print("Cut wav")
-            return self._cut_audio(min_clip_duration_ms)
-        return tf.convert_to_tensor(wav, dtype=tf.float32)
+    def time_masking_rand(self, min_mask: int = 1000, max_mask: int = 5000) -> tf.Tensor:
+        if tf.rank(self._audio_tensor) != 1:
+            return self._audio_tensor
 
+        max_mask_length = random.randint(min_mask, max_mask)
 
-    # def change_amplitude_randomize(self, min: float = 0.5, max: float = 5):
-    #     return self._wav_tensor * random.uniform(min, max)
+        if tf.shape(self._audio_tensor)[0] <= max_mask_length:
+            return self._audio_tensor
+
+        mask_length = tf.random.uniform([], maxval=max_mask_length, dtype=tf.int32)
+
+        mask_start_max = tf.shape(self._audio_tensor)[0] - mask_length
+        mask_start = tf.random.uniform([], maxval=mask_start_max, dtype=tf.int32)
+
+        mask = tf.concat([
+            tf.ones([mask_start]),
+            tf.zeros([mask_length]),
+            tf.ones([tf.shape(self._audio_tensor)[0] - mask_start - mask_length])
+        ], axis=0)
+
+        return self._audio_tensor * mask
+
+    def change_pitch_rand(self, sample_rate=48_000, min_shift=2, max_shift=4) -> tf.Tensor:
+        pitch_shift = random.randint(min_shift, max_shift)
+        pitched_audio = librosa.effects.pitch_shift(self._audio_np, sr=sample_rate, n_steps=pitch_shift)
+        return tf.convert_to_tensor(pitched_audio, dtype=tf.float32)
+
+    def change_speed_rand(self, min_factor: float = 0.5, max_factor: float = 2.0) -> tf.Tensor:
+        speed_factor = random.uniform(min_factor, max_factor)
+        stretched_audio = librosa.effects.time_stretch(self._audio_np, rate=speed_factor)
+        return tf.convert_to_tensor(stretched_audio, dtype=tf.float32)
+
+    def create_spectrogram(self):
+        spectrogram = tf.signal.stft(self._audio_tensor, frame_length=320, frame_step=32)
+        spectrogram = tf.abs(spectrogram)
+        spectrogram = tf.expand_dims(spectrogram, axis=2)
+        return spectrogram
+
 
 
 
